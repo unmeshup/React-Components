9a9ae4e005c7c0a5571197711a0ce721d8cf1818
unmeshup <unplfc@gmail.com>
2024-02-05 18:13:50 +0530

    Sixth Component

---
+++ table/src/UserData.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const UserData = ({ users }) => {
+  return (
+    <>
+      {users.map((curUser) => {
+        const { id, name, email } = curUser;
+        const { street, city, zipcode } = curUser.address;
+
+        return (
+          <tr key={id}>
+            <td>{id}</td>
+            <td>{name}</td>
+            <td>{email}</td>
+            <td>
+              {street}, {city}, , {zipcode}
+            </td>
+          </tr>
+        );
+      })}
+    </>
+  );
+};
+
+export default UserData;
